155a5950091dba2e1edbee6d7ed888b2f0a2c467
nornso <wusu112358@gmail.com>
2021-01-19 02:35:04 +0800

    添加URP

---
+++ Assets/Scripts/Graph.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Graph : MonoBehaviour
+{
+    [SerializeField]
+    Transform pointPrefab = default;
+
+    [SerializeField, Range(10, 100)]
+    int resolution = 10;
+
+
+    private void Awake()
+    {
+		float step = 2f / resolution;
+		var scale = Vector3.one * step;
+		var position = Vector3.zero;
+		for (int i = 0; i < resolution; i++)
+		{
+			Transform point = Instantiate(pointPrefab);
+			position.x = (i + 0.5f) * step - 1f;
+			position.y = position.x * position.x * position.x;
+			point.localPosition = position;
+			point.localScale = scale;
+			point.SetParent(transform, false);
+		}
+	}
+}
