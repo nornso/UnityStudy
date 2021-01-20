4760332f4a380f0dd9e55ccd699ee4f3d629a186
nornso <wusu112358@gmail.com>
2021-01-20 16:15:31 +0800

    Function Library

---
+++ Assets/Scripts/FunctionLibrary.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using static UnityEngine.Mathf;
+
+public static class FunctionLibrary
+{
+    public delegate float Function(float x, float t);
+
+    public enum FunctionName { Wave, MultiWave, Ripple }
+
+    static Function[] functions = { Wave, MultiWave, Ripple };
+
+    public static float Wave(float x, float t)
+    {
+        return Sin(PI * (x + t));
+    }
+
+    public static float MultiWave(float x, float t)
+    {
+        float y = Sin(PI * (x + 0.5f * t));
+        y += Sin(2f * PI * (x + t)) * 0.5f;
+        return y * (2f / 3f);
+    }
+
+    public static float Ripple(float x, float t)
+    {
+        float d = Abs(x);
+        float y = Sin(PI * (4f * d - t));
+
+        return y / (1f + 10f * d);
+    }
+
+    public static Function GetFunction(FunctionName index)
+    {
+        return functions[(int)index];
+    }
+}
--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -11,19 +11,45 @@ public class Graph : MonoBehaviour
     int resolution = 10;
 
 
-    private void Awake()
+    [SerializeField]
+    FunctionLibrary.FunctionName function = default;
+
+    Transform[] points;
+
+    void Awake()
     {
-		float step = 2f / resolution;
-		var scale = Vector3.one * step;
-		var position = Vector3.zero;
-		for (int i = 0; i < resolution; i++)
-		{
-			Transform point = Instantiate(pointPrefab);
-			position.x = (i + 0.5f) * step - 1f;
-			position.y = position.x * position.x * position.x;
-			point.localPosition = position;
-			point.localScale = scale;
-			point.SetParent(transform, false);
-		}
-	}
+        float step = 2f / resolution;
+        var scale = Vector3.one * step;
+        var position = Vector3.zero;
+
+        points = new Transform[resolution];
+
+        for (int i = 0; i < resolution; i++)
+        {
+            Transform point = Instantiate(pointPrefab);
+            points[i] = point;
+            position.x = (i + 0.5f) * step - 1f;
+            //position.y = position.x * position.x * position.x;
+            point.localPosition = position;
+            point.localScale = scale;
+            point.SetParent(transform, false);
+        }
+    }
+
+    void Update()
+    {
+        float time = Time.time;
+        FunctionLibrary.Function f = FunctionLibrary.GetFunction(function);
+
+        for (int i = 0; i < points.Length; i++)
+        {
+            Transform point = points[i];
+            Vector3 position = point.localPosition;
+
+            if(f != null)
+                position.y = f(position.x, time);
+
+            point.localPosition = position;
+        }
+    }
 }
