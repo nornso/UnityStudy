f1686dc413c6591129ba85f239df7f8213d2e6d4
nornso <wusu112358@gmail.com>
2021-03-25 01:49:10 +0800

    Compute Shader 1.0

---
+++ Assets/Scripts/GPUGraph.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GPUGraph : MonoBehaviour
+{
+    [SerializeField, Range(10, 200)]
+    int resolution = 10;
+
+    [SerializeField, Min(0f)]
+    float functionDuration = 1f, transitionDuration = 1f;
+
+    [SerializeField]
+    FunctionLibrary.FunctionName function = default;
+
+    [SerializeField]
+    TransitionMode transtionMode = TransitionMode.Cycle;
+
+    public enum TransitionMode { Cycle, Rnadom };
+
+    private float duration;
+
+    private bool transitioning;
+
+    FunctionLibrary.FunctionName transitonFunciton;
+
+    ComputeBuffer positionBuffer;
+
+    private void OnEnable()
+    {
+        positionBuffer = new ComputeBuffer(resolution * resolution, 3 * 4);
+    }
+
+    private void OnDisable()
+    {
+        positionBuffer = new ComputeBuffer(resolution * resolution, 3 * 4);
+        positionBuffer = null;
+    }
+
+    void Update()
+    {
+    }
+
+    private void PickNexFunctipn()
+    {
+        function = transtionMode == TransitionMode.Cycle ?
+                   FunctionLibrary.GetNextFunctionName(function) :
+                   FunctionLibrary.GetRandomNextFunctionNameOtherThan(function);
+    }
+}
--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -7,7 +7,7 @@ public class Graph : MonoBehaviour
     [SerializeField]
     Transform pointPrefab = default;
 
-    [SerializeField, Range(10, 100)]
+    [SerializeField, Range(10, 200)]
     int resolution = 10;
 
     [SerializeField, Min(0f)]
